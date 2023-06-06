c254357b4d56c273cccfb6c5fca712090f226139
KumachiTL <135353127+KumachiTL@users.noreply.github.com>
2023-06-07 00:40:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// JavaScript code to toggle the dropdown submenu
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const menu = dropdown.querySelector('.menu');
+  const submenu = dropdown.querySelector('.dropdown-content');
+
+  menu.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent default link behavior
+    submenu.classList.toggle('show');
+  });
+
+  // Close the dropdown if clicked outside
+  document.addEventListener('click', event => {
+    if (!dropdown.contains(event.target)) {
+      submenu.classList.remove('show');
+    }
+  });
+});
+
+// Search
+
+const searchButton = document.getElementById('search-btn');
+const searchBar = document.getElementById('search-bar');
+
+searchButton.addEventListener('click', () => {
+  searchButton.classList.toggle('active');
+  searchBar.classList.toggle('active');
+
+  if (searchButton.classList.contains('active')) {
+    searchBar.focus();
+  } else {
+    searchBar.value = '';
+  }
+});
+
+// Change Chapter
+
+function navigateToLink() {
+  var dropdown = document.getElementById("dropdown");
+  var selectedOption = dropdown.value;
+  if (selectedOption) {
+    window.location.href = selectedOption;
+  }
+}
